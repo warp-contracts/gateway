9ef5f6484e7d2a1ceb53c3f59e6782542a27997e
ppedziwiatr <peter@redstone.finance>
2021-12-10 15:53:31 +0100

    chore: connection pool settings

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -7,5 +7,15 @@ export const connect = (port: number, type: string, baseDir: string): Knex => {
       filename: `${baseDir}/db-${type}-${port}.sqlite`,
     },
     useNullAsDefault: true,
+    pool: {
+      "min": 3,
+      "max": 10,
+      "createTimeoutMillis": 3000,
+      "acquireTimeoutMillis": 30000,
+      "idleTimeoutMillis": 30000,
+      "reapIntervalMillis": 1000,
+      "createRetryIntervalMillis": 100,
+      "propagateCreateError": false
+    },
   });
 };
