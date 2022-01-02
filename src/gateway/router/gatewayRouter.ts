ec45911c7201c2afa3875ab0da9e8d856aca897a
ppedziwiatr <peter@redstone.finance>
2022-01-02 14:06:42 +0100

    feat: stats endpoint

--- src/gateway/router/gatewayRouter.ts
+++ src/gateway/router/gatewayRouter.ts
@@ -2,11 +2,13 @@ import Router from "@koa/router";
 import {contractsRoute} from "./routes/contractsRoute";
 import {interactionsRoute} from "./routes/interactionsRoute";
 import {searchRoute} from "./routes/searchRoute";
+import {statsRoute} from "./routes/statsRoute";
 
 const gatewayRouter = new Router({prefix: '/gateway'});
 
 gatewayRouter.get("/contracts", contractsRoute);
 gatewayRouter.get("/search/:phrase", searchRoute);
 gatewayRouter.get("/interactions", interactionsRoute);
+gatewayRouter.get("/stats", statsRoute);
 
 export default gatewayRouter;
