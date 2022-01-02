242074f9d61527dc333dad2c1154a9ace6d67379
ppedziwiatr <peter@redstone.finance>
2022-01-02 16:42:54 +0100

    feat: transactions per day stats

--- src/gateway/router/gatewayRouter.ts
+++ src/gateway/router/gatewayRouter.ts
@@ -3,6 +3,7 @@ import {contractsRoute} from "./routes/contractsRoute";
 import {interactionsRoute} from "./routes/interactionsRoute";
 import {searchRoute} from "./routes/searchRoute";
 import {statsRoute} from "./routes/statsRoute";
+import {statsTxPerDayRoute} from "./routes/statsTxPerDayRoute";
 
 const gatewayRouter = new Router({prefix: '/gateway'});
 
@@ -10,5 +11,6 @@ gatewayRouter.get("/contracts", contractsRoute);
 gatewayRouter.get("/search/:phrase", searchRoute);
 gatewayRouter.get("/interactions", interactionsRoute);
 gatewayRouter.get("/stats", statsRoute);
+gatewayRouter.get("/stats/per-day", statsTxPerDayRoute);
 
 export default gatewayRouter;
