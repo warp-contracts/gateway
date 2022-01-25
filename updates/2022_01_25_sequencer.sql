f8f2527cde34f40007b10385b44ee5307a9be2d4
ppedziwiatr <peter@redstone.finance>
2022-01-25 15:49:21 +0100

    fix: adding salt to sort key

--- updates/2022_01_25_sequencer.sql
+++ updates/2022_01_25_sequencer.sql
@@ -3,3 +3,6 @@ ALTER TABLE interactions
 
 ALTER TABLE sequencer
     RENAME COLUMN bundled_tx_id TO bundler_tx_id;
+
+ALTER TABLE sequencer
+    ADD COLUMN sequence_sort_key TEXT NOT NULL DEFAULT '';
