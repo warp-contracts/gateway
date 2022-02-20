aed8d1bff07d8d88852f4ae7bd5002103f56a0ed
ppedziwiatr <peter@redstone.finance>
2022-02-20 18:50:17 +0100

    feat: support for wasm contracts

--- src/gateway/router/routes/contractRoute.ts
+++ src/gateway/router/routes/contractRoute.ts
@@ -15,13 +15,15 @@ export async function contractRoute(ctx: Router.RouterContext) {
     const benchmark = Benchmark.measure();
     const result: any = await gatewayDb.raw(
       `
-          SELECT contract_id as "txId",
-                 src_tx_id as "srcTxId",
-                 src as src,
-                 init_state as "initState",
-                 owner as owner, 
-                 pst_ticker as "pstTicker",
-                 pst_name as "pstName"
+          SELECT contract_id                                                                     as "txId",
+                 src_tx_id                                                                       as "srcTxId",
+                 (case when src_content_type = 'application/javascript' then src else null end)  as src,
+                 (case when src_content_type = 'application/wasm' then src_binary else null end) as "srcBinary",
+                 init_state                                                                      as "initState",
+                 owner                                                                           as "owner",
+                 pst_ticker                                                                      as "pstTicker",
+                 pst_name                                                                        as "pstName",
+                 src_wasm_lang                                                                   as "srcWasmLang"
           FROM contracts
           WHERE contract_id = ?;
       `, [id]
