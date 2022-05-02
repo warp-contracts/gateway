373a736b2d1352407d7dcad44f2e4e4c14246d12
ppe <peter@redstone.finance>
2022-05-02 10:51:43 +0200

    fix: safeContracts db query fix

--- src/gateway/router/routes/safeContractsRoute.ts
+++ src/gateway/router/routes/safeContractsRoute.ts
@@ -11,10 +11,11 @@ export async function safeContractsRoute(ctx: Router.RouterContext) {
           SELECT i.contract_id, count(i) AS interactions
           FROM contracts c
                    JOIN interactions i ON i.contract_id = c.contract_id
+                   JOIN contracts_src s ON s.src_tx_id = c.src_tx_id
           WHERE 
-              (c.src_content_type = 'application/javascript' 
-                   AND (c.src NOT LIKE '%readContractState%' AND c.src NOT LIKE '%unsafeClient%'))
-          OR c.src_content_type = 'application/wasm'
+              (s.src_content_type = 'application/javascript' 
+                   AND (s.src NOT LIKE '%readContractState%' AND s.src NOT LIKE '%unsafeClient%'))
+          OR s.src_content_type = 'application/wasm'
           GROUP BY i.contract_id
           HAVING count(i) < 20000 AND count(i) >= 1
           ORDER BY count(i) DESC;
