e6e82799b16dc37dfb3d3359d4f0b28a9171a3cd
asiaziola <ziola.jm@gmail.com>
2022-06-03 15:20:38 +0200

    feat: loading contract source with loadContractSource sdk method

--- src/gateway/tasks/evolvedContractSources.ts
+++ src/gateway/tasks/evolvedContractSources.ts
@@ -1,4 +1,4 @@
-import { ContractType, getTag, SmartWeaveTags, WasmSrc } from "redstone-smartweave";
+import { ContractDefinitionLoader, ContractSource } from "redstone-smartweave";
 import {GatewayContext} from "../init";
 import { TaskRunner } from "./TaskRunner";
 
@@ -11,95 +11,65 @@ export async function runEvolvedContractsSourceTask(context: GatewayContext) {
 }
 
 async function loadEvolvedContractsSource (context: GatewayContext) {
-    const {logger, gatewayDb, arweaveWrapper} = context;
+  const {logger, gatewayDb, arweaveWrapper, arweave} = context;
+  const definitionLoader = new ContractDefinitionLoader(arweave);
   
-    const result: { evolve: string }[] = (await gatewayDb.raw(
-        `
-            SELECT evolve
-            FROM interactions
-            WHERE evolve NOT IN (SELECT src_tx_id from contracts_src)
-            AND evolve IS NOT NULL;
-        `
-      )).rows;
+  const result: { evolve: string }[] = (await gatewayDb.raw(
+      `
+          SELECT evolve
+          FROM interactions
+          WHERE evolve NOT IN (SELECT src_tx_id from contracts_src)
+          AND evolve IS NOT NULL;
+      `
+    )).rows;
     
-      const missing = result?.length || 0;
-      logger.info(`Loading ${missing} evolved contract sources.`);
+  const missing = result?.length || 0;
+  logger.info(`Loading ${missing} evolved contract sources.`);
     
-      if (missing == 0) {
-        return;
-      }
+  if (missing == 0) {
+    return;
+  }
   
   for (const row of result) {
     logger.debug(`Loading evolved contract source: ${row.evolve}.`);
-
     const srcTxId = row.evolve;
 
-        try {        
-            const contractSrcTx = await arweaveWrapper.tx(srcTxId);
-
-            const srcContentType = getTag(contractSrcTx, SmartWeaveTags.CONTENT_TYPE);
-
-            const supportedSrcContentTypes = ['application/javascript', 'application/wasm'];
-
-            if (supportedSrcContentTypes.indexOf(srcContentType) == -1) {
-              throw new Error(`Contract source content type ${srcContentType} not supported`);
-            }
+    try {  
+      const {src, srcWasmLang, contractType, srcTx}: ContractSource = await definitionLoader.loadContractSource(srcTxId);
         
-            const src =
-            srcContentType == 'application/javascript'
-                ? await arweaveWrapper.txDataString(srcTxId)
-                : await arweaveWrapper.txData(srcTxId);
-        
-            let srcWasmLang;
-            let wasmSrc: WasmSrc;
-
-            if (srcContentType == 'application/wasm') {
-              wasmSrc = new WasmSrc(src as Buffer);
-              srcWasmLang = getTag(contractSrcTx, SmartWeaveTags.WASM_LANG);
-
-              if (!srcWasmLang) {
-                throw new Error(`Wasm lang not set for wasm contract src ${srcTxId}`);
-              }
-            }
-        
-            let contracts_src_insert: any = {
-              src_tx_id: srcTxId,
-              src_content_type: srcContentType,
-              src_tx: contractSrcTx.toJSON()
-            }
+      let contracts_src_insert: any = {
+        src_tx_id: srcTxId,
+        src_content_type: contractType == 'js'
+        ? 'application/javascript'
+        : 'application/wasm',
+        src_tx: srcTx
+      }
       
-            if (srcContentType == 'application/javascript') {
-              contracts_src_insert = {
-                ...contracts_src_insert,
-                src: src
-              }
-            } else {
-              const rawTxData = await arweaveWrapper.txData(srcTxId);
-              contracts_src_insert = {
-                ...contracts_src_insert,
-                src_binary: rawTxData,
-                src_wasm_lang: srcWasmLang
-              }
-            }
+      if (contractType == 'js') {
+        contracts_src_insert = {
+          ...contracts_src_insert,
+          src: src
+        }
+      } else {
+        const rawTxData = await arweaveWrapper.txData(srcTxId);
+        contracts_src_insert = {
+          ...contracts_src_insert,
+          src_binary: rawTxData,
+          src_wasm_lang: srcWasmLang
+        }
+      }
 
-            logger.debug(`Inserting ${row.evolve} evolved contract source into db`);
+      logger.debug(`Inserting ${row.evolve} evolved contract source into db`);
 
-            await gatewayDb("contracts_src")
-              .insert(contracts_src_insert)
-              .onConflict("src_tx_id")
-              .merge([
-                'src',
-                'src_content_type',
-                'src_binary',
-                'src_wasm_lang',
-                'bundler_src_tx_id',
-                'bundler_src_node',
-                'src_tx']);
+      await gatewayDb("contracts_src")
+        .insert(contracts_src_insert)
+        .onConflict("src_tx_id")
+        .ignore();
       
-            logger.debug(`${row.evolve} evolved contract source inserted into db`);
+      logger.debug(`${row.evolve} evolved contract source inserted into db`);
 
-          } catch (e) {
-            logger.error(`Error while loading evolved contract source ${srcTxId}`, e);
-          }
+    } catch (e) {
+      logger.error(`Error while loading evolved contract source ${srcTxId}`, e);
     }
   }
+}
