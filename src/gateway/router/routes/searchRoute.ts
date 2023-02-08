c4f64179964cbcb20a090a93887f51d6cec3f7db
Tadeuchi <tadeuszwl@gmail.com>
2023-02-08 13:02:59 +0100

    fix: quick search - exact matching

--- src/gateway/router/routes/searchRoute.ts
+++ src/gateway/router/routes/searchRoute.ts
@@ -1,10 +1,11 @@
 import Router from '@koa/router';
 import { Benchmark } from 'warp-contracts';
+import { isTxIdValid } from '../../../utils';
 
 export async function searchRoute(ctx: Router.RouterContext) {
   const { logger, gatewayDb } = ctx;
 
-  const { phrase } = ctx.params;
+  const phrase = ctx.params.phrase.trim();
   const { testnet } = ctx.query;
 
   if (phrase?.length < 3) {
@@ -12,26 +13,48 @@ export async function searchRoute(ctx: Router.RouterContext) {
     return;
   }
 
+  const validTs = isTxIdValid(phrase);
+
   try {
     const benchmark = Benchmark.measure();
-    const result: any = await gatewayDb.raw(
-      `
-          SELECT 1 as sort_order, contract_id as id, 'pst' as type, '{}'::jsonb as interaction, '' as confirmation_status, pst_ticker, pst_name
-          FROM contracts
-          WHERE pst_ticker ILIKE ? OR pst_name ILIKE ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
-          UNION ALL
+    let result: any;
+    if (validTs) {
+        result = await gatewayDb.raw(
+            `
           SELECT 2 as sort_order, contract_id as id, 'contract' as type, '{}'::jsonb as interaction, '' as confirmation_status, '' as pst_ticker, '' as pst_name
           FROM contracts
-          WHERE contract_id ILIKE ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
+          WHERE contract_id = ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
           GROUP BY contract_id, type
           UNION ALL
           SELECT 3 as sort_order, interaction_id as id, 'interaction' as type, interaction, confirmation_status, '' as pst_ticker, '' as pst_name
           FROM interactions
-          WHERE interaction_id ILIKE ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
+          WHERE interaction_id = ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
           UNION ALL
           SELECT 4 as sort_order, src_tx_id as id, 'source' as type, '{}'::jsonb as interaction, '' as confirmation_status, '' as pst_ticker, '' as pst_name
           FROM contracts_src
-          WHERE src_tx_id ILIKE ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'} AND src != 'error'
+          WHERE src_tx_id = ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'} AND src != 'error'
+          UNION ALL
+          SELECT 5 as sort_order, creator_id as id, 'creator' as type, '{}'::jsonb as interaction, '' as confirmation_status, '' as pst_ticker, '' as pst_name
+          FROM
+            (
+              WITH temp_creator AS (
+                SELECT DISTINCT owner as creator_id from interactions where owner = ?
+              UNION ALL
+                SELECT DISTINCT owner as creator_id FROM contracts WHERE owner = ?
+            )
+          SELECT DISTINCT * FROM temp_creator) AS creator
+          ORDER BY sort_order
+          LIMIT 30;
+      `,
+            [phrase, phrase, phrase, phrase, phrase]
+        );
+
+    } else {
+        result = await gatewayDb.raw(
+            `
+          SELECT 1 as sort_order, contract_id as id, 'pst' as type, '{}'::jsonb as interaction, '' as confirmation_status, pst_ticker, pst_name
+          FROM contracts
+          WHERE pst_ticker ILIKE ? OR pst_name ILIKE ? AND testnet IS ${testnet == 'true' ? `NOT NULL` : 'NULL'}
           UNION ALL
           SELECT 5 as sort_order, creator_id as id, 'creator' as type, '{}'::jsonb as interaction, '' as confirmation_status, '' as pst_ticker, '' as pst_name
           FROM
@@ -45,8 +68,10 @@ export async function searchRoute(ctx: Router.RouterContext) {
           ORDER BY sort_order
           LIMIT 30;
       `,
-      [`${phrase}%`, `${phrase}%`, `${phrase}%`, `${phrase}%`, `${phrase}%`, `${phrase}%`, `${phrase}%`]
-    );
+      [`${phrase}%`, `${phrase}%`, `${phrase}%`, `${phrase}%`]
+        );
+    }
+
     ctx.body = result?.rows;
     logger.debug('Contracts loaded in', benchmark.elapsed());
   } catch (e: any) {
