1801356672520d59092551d46b71063e8f03050f
Tadeuchi <tadeuszwl@gmail.com>
2023-02-09 14:24:30 +0100

    fix: interactions stats per -  block_timestamp, not null, idx

--- src/gateway/router/routes/stats/txsPerDayRoute.ts
+++ src/gateway/router/routes/stats/txsPerDayRoute.ts
@@ -11,7 +11,8 @@ export async function txsPerDayRoute(ctx: Router.RouterContext) {
       `
         WITH contracts_per_day AS (
             SELECT date(to_timestamp((block_timestamp)::integer)) as date, contract_id as interaction
-            FROM contracts ${testnet ? ' WHERE testnet IS NOT NULL' : ''}
+            FROM contracts 
+            WHERE testnet ${testnet ? ' IS NOT NULL' : ' IS NULL'}
         )
         SELECT date, count(*) as per_day FROM contracts_per_day
         GROUP BY date
@@ -21,8 +22,9 @@ export async function txsPerDayRoute(ctx: Router.RouterContext) {
     const interactions: any = await gatewayDb.raw(
       `
         WITH transactions_per_day AS (
-            SELECT date(to_timestamp((interaction->'block'->>'timestamp')::integer)) as date, interaction_id as interaction
-            FROM interactions ${testnet ? ' WHERE testnet IS NOT NULL' : ''}
+            SELECT date(to_timestamp((block_timestamp)::integer)) as date, interaction_id as interaction
+            FROM interactions 
+            WHERE testnet ${testnet ? ' IS NOT NULL' : ' IS NULL'}
         )
         SELECT date, count(*) as per_day FROM transactions_per_day
         GROUP BY date
