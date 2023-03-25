184ed5f7ce21e6e97c11975ab360115ede0b64e6
ppe <peter@redstone.finance>
2023-03-25 22:04:04 +0100

    chore: yolo

--- src/gateway/tasks/contractsMetadata.ts
+++ src/gateway/tasks/contractsMetadata.ts
@@ -70,7 +70,7 @@ async function loadContractsFromGql(context: GatewayContext) {
 
   const currentNetworkHeight = getCachedNetworkData().cachedNetworkInfo.height;
   lastProcessedBlockHeight = lastProcessedBlockHeight || FIRST_SW_TX_BLOCK_HEIGHT;
-  const from = lastProcessedBlockHeight - 5;
+  const from = lastProcessedBlockHeight - 1;
   const to = currentNetworkHeight - from <= 10 ? currentNetworkHeight : from + 10;
   logger.debug('Load contracts params', {
     from,
