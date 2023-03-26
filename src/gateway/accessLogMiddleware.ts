3c8ef6e309d00154b143f71b670ce87b888f12da
ppe <peter@redstone.finance>
2023-03-26 13:45:29 +0200

    fix: don't log health checks...

--- src/gateway/accessLogMiddleware.ts
+++ src/gateway/accessLogMiddleware.ts
@@ -1,18 +1,18 @@
 import * as util from "util";
-import {format} from 'date-fns'
 import {DefaultState, Next, ParameterizedContext} from "koa";
 import {GatewayContext} from "./init";
 
-const LOG_FORMAT = '%s [%s] "%s %s HTTP/%s" %d %s';
-const DATE_FORMAT = 'd/MMM/yyyy:HH:mm:ss xx';
+const LOG_FORMAT = '%s "%s %s HTTP/%s" %d %s';
 
 export async function accessLogMiddleware(ctx: ParameterizedContext<DefaultState, GatewayContext, any>, next: Next): Promise<void> {
   await next();
   try {
+    if (ctx.path == '/gateway/gcp/alive') {
+      return;
+    }
     ctx.accessLogger.debug(util.format(
         LOG_FORMAT,
         ctx.ip,
-        format(new Date(), DATE_FORMAT),
         ctx.method,
         ctx.path,
         ctx.req.httpVersion,
