fa637d318a68ef86da34fbd033f82c45933f4679
ppe <peter@redstone.finance>
2023-04-12 10:56:19 +0200

    restore task

--- src/gateway/runGatewayTasks.ts
+++ src/gateway/runGatewayTasks.ts
@@ -55,5 +55,5 @@ export async function runGatewayTasks(context: GatewayContext) {
 
   await runEvolvedContractSourcesTask(context);
 
-  // await runSyncLastSixHoursTransactionsTask(context);
+  await runSyncLastSixHoursTransactionsTask(context);
 }
