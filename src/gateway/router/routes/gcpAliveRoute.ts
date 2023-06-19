c4461bb10c821c8ab1d7e9fd1a55939960f2df66
Asia <ziola.jm@gmail.com>
2023-06-19 13:42:41 +0200

    fix: get chached network info only if not in replica (#171)

--- src/gateway/router/routes/gcpAliveRoute.ts
+++ src/gateway/router/routes/gcpAliveRoute.ts
@@ -1,36 +1,38 @@
 import Router from '@koa/router';
-import {getCachedNetworkData} from "../../tasks/networkInfoCache";
+import { getCachedNetworkData } from '../../tasks/networkInfoCache';
 
 export async function gcpAliveRoute(ctx: Router.RouterContext) {
-  const cachedNetworkData = getCachedNetworkData();
-  const arBlockHeight = cachedNetworkData.cachedBlockInfo.height;
+  const arBlockHeight = ctx.replica ? null : getCachedNetworkData().cachedBlockInfo.height;
 
   ctx.body = {
-    'gateway': 'ok',
-    'ar_block_height': arBlockHeight,
-    'db': await dbAccessible(ctx, arBlockHeight)
-  }
+    gateway: 'ok',
+    db: await dbAccessible(ctx, arBlockHeight),
+    ...(arBlockHeight && { ar_block_height: arBlockHeight }),
+  };
 }
 
-async function dbAccessible(ctx: Router.RouterContext, arBlockHeight: number) {
+async function dbAccessible(ctx: Router.RouterContext, arBlockHeight: number | null) {
   const { dbSource } = ctx;
 
   if (dbSource.healthCheckEnabled()) {
     try {
-      const result = await dbSource.healthCheck(
-        `select max(block_height) as l1_max_bh
+      const result = await dbSource
+        .healthCheck(
+          `select max(block_height) as l1_max_bh
          from interactions
-         where source = 'arweave';`).timeout(500, {cancel: true});
+         where source = 'arweave';`
+        )
+        .timeout(500, { cancel: true });
       return {
         up: 1,
         l1_last_interaction_height: result.rows[0].l1_max_bh,
-        l1_interaction_height_diff: arBlockHeight - result.rows[0].l1_max_bh
-      }
+        ...(arBlockHeight && { l1_interaction_height_diff: arBlockHeight - result.rows[0].l1_max_bh }),
+      };
     } catch (e: any) {
       return {
         up: 0,
-        error: e.message
-      }
+        error: e.message,
+      };
     }
   }
 
