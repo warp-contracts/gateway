71a445c52a8a169f3862debb9d23d5c4ac11fbc5
Asia <ziola.jm@gmail.com>
2023-11-16 13:47:07 +0100

    contracts-by-source route - remove interactions count (#186)

--- src/gateway/router/routes/contracts/contractsBySourceRoute.ts
+++ src/gateway/router/routes/contracts/contractsBySourceRoute.ts
@@ -1,14 +1,14 @@
 import Router from '@koa/router';
-import {Benchmark} from 'warp-contracts';
-import {isTxIdValid} from '../../../../utils';
-import {GatewayError} from "../../../errorHandlerMiddleware";
+import { Benchmark } from 'warp-contracts';
+import { isTxIdValid } from '../../../../utils';
+import { GatewayError } from '../../../errorHandlerMiddleware';
 
 const MAX_INTERACTIONS_PER_PAGE = 5000;
 
 export async function contractsBySourceRoute(ctx: Router.RouterContext) {
-  const {logger, dbSource} = ctx;
+  const { logger, dbSource } = ctx;
 
-  const {id, page, limit, sort} = ctx.query;
+  const { id, page, limit, sort } = ctx.query;
 
   const parsedPage = page ? parseInt(page as string) : 1;
 
@@ -29,8 +29,8 @@ export async function contractsBySourceRoute(ctx: Router.RouterContext) {
 
   const benchmark = Benchmark.measure();
 
-    const result: any = await dbSource.raw(
-      `
+  const result: any = await dbSource.raw(
+    `
           with c as (select contract_id, owner, bundler_contract_tx_id, block_height, block_timestamp
                      from contracts
                      where src_tx_id = ?
@@ -41,26 +41,20 @@ export async function contractsBySourceRoute(ctx: Router.RouterContext) {
               src as (select count(*) AS total
                 from contracts
                 where src_tx_id = ?
-                  and type <> 'error'),
+                  and type <> 'error')
               
-              interactions as (select c.contract_id, count(*) as interactions
-          from c
-              join interactions on interactions.contract_id = c.contract_id
-          group by c.contract_id)
           SELECT c.contract_id               AS "contractId",
                  c.owner                     AS "owner",
                  c.bundler_contract_tx_id    AS "bundlerTxId",
                  c.block_height              AS "blockHeight",
                  c.block_timestamp           AS "blockTimestamp",
-                 coalesce(i.interactions, 0) AS "interactions",
                  coalesce(src.total, 0)      AS "total"
           from c
-                   LEFT JOIN interactions i ON c.contract_id = i.contract_id
                    LEFT JOIN src ON TRUE
               ${sort == 'desc' || sort == 'asc' ? `ORDER BY c.block_height ${sort.toUpperCase()}, c.contract_id` : ''};
         `,
-      bindings
-    );
+    bindings
+  );
 
   const total = result?.rows?.length > 0 ? result?.rows[0].total : 0;
 
