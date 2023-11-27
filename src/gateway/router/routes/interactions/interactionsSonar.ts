5188a8d4f10157e66aee21d84c93c989ee9e4f88
Asia <ziola.jm@gmail.com>
2023-11-27 12:17:45 +0100

    interactions sonar - input in data update

--- src/gateway/router/routes/interactions/interactionsSonar.ts
+++ src/gateway/router/routes/interactions/interactionsSonar.ts
@@ -1,12 +1,13 @@
 import Router from '@koa/router';
-import {Benchmark} from 'warp-contracts';
+import { Benchmark } from 'warp-contracts';
+import { addInputToInteractionTags } from './interactionsSortKeyRoute_v2';
 
 const MAX_INTERACTIONS_PER_PAGE = 5000;
 
 export async function interactionsSonar(ctx: Router.RouterContext) {
-  const {dbSource, logger} = ctx;
+  const { dbSource, logger } = ctx;
 
-  const {contractId, confirmationStatus, page, limit, from, to, source} = ctx.query;
+  const { contractId, confirmationStatus, page, limit, from, to, source } = ctx.query;
 
   const parsedPage = page ? parseInt(page as string) : 1;
 
@@ -32,6 +33,7 @@ export async function interactionsSonar(ctx: Router.RouterContext) {
 
   const query = `
       SELECT interaction,
+             input,
              confirmation_status,
              sort_key,
              confirming_peer,
@@ -40,9 +42,9 @@ export async function interactionsSonar(ctx: Router.RouterContext) {
       FROM interactions
       WHERE (contract_id = ? OR interact_write @> ARRAY [?])
           ${
-                  parsedConfirmationStatus
-                          ? ` AND confirmation_status IN (${parsedConfirmationStatus.map((status) => `'${status}'`).join(', ')})`
-                          : ''
+            parsedConfirmationStatus
+              ? ` AND confirmation_status IN (${parsedConfirmationStatus.map((status) => `'${status}'`).join(', ')})`
+              : ''
           } ${from ? ' AND sort_key > ?' : ''} ${to ? ' AND sort_key <= ?' : ''} ${source ? `AND source = ?` : ''}
       ORDER BY sort_key DESC ${parsedPage ? ' LIMIT ? OFFSET ?' : ''};
   `;
@@ -85,18 +87,20 @@ export async function interactionsSonar(ctx: Router.RouterContext) {
       },
     }),
 
-    interactions: result?.rows?.map((r: any) => ({
-      status: r.confirmation_status,
-      confirming_peers: r.confirming_peer,
-      confirmations: r.confirmations,
-      interaction: {
-        ...r.interaction,
-        bundlerTxId: r.bundler_tx_id,
-        sortKey: r.sort_key,
-      },
-    })),
+    interactions: result?.rows?.map((r: any) => {
+      const interactionTagsWithInput = addInputToInteractionTags(r.interaction.tags, r.input);
+      return {
+        status: r.confirmation_status,
+        confirming_peers: r.confirming_peer,
+        confirmations: r.confirmations,
+        interaction: {
+          ...{ ...r.interaction, tags: interactionTagsWithInput },
+          bundlerTxId: r.bundler_tx_id,
+          sortKey: r.sort_key,
+        },
+      };
+    }),
   };
 
   logger.info('Mapping interactions: ', benchmark.elapsed());
- 
 }
